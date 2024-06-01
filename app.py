66a2ac2ee633e064234b339d357585b02ff32abe
sourabh2104 <sourabhyadav1256@gmail.com>
2024-06-02 01:42:11 +0530

    README.md file updated
    Signed-off-by: sourabh2104 <sourabhyadav1256@gmail.com>

---
+++ app.py
@@ -0,0 +1,3 @@
+from src.helper import main
+
+main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Medical Disease Analysis Project',
+    version= '0.0.0',
+    author= 'Sourabh Yadav',
+    author_email= 'sourabhyadav1256@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
